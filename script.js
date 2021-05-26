27b2646f9244f08e86f40fa920e1a81978170293
leandroaragao31 <leandroaragao31@gmail.com>
2021-05-25 21:09:44 -0300

    criando lista de loop com for/while

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//resposta 1
+for(let loop = 1; loop <=10; loop ++){
+    console.log(loop)
+}
+
+//resposta 2
+
+let dados = 10
+
+while(dados >=1){
+    console.log(dados)
+    dados--
+}
+
+//resposta 3
+
+for(impar = 1; impar <=100; impar+=2){
+    console.log(impar)
+}
+
+//resposta 4
+
+for(par =0; par <=100; par+=2){
+    console.log(par)
+}
